87f088637efd0d7fc9c8c27825950bd11dde9106
SubhrajyotiSen <subhrajyoti12@gmail.com>
2023-10-05 21:57:43 +0530

    Update projects

--- source-code/final-project/app/src/main/java/com/droidcon/composepreview/ui/composables/BorrowedItemRowPreviews.kt
+++ source-code/final-project/app/src/main/java/com/droidcon/composepreview/ui/composables/BorrowedItemRowPreviews.kt
@@ -8,13 +8,7 @@ import androidx.compose.ui.tooling.preview.PreviewParameter
 import com.droidcon.composepreview.data.models.BorrowItem
 import com.droidcon.composepreview.ui.theme.BorrowTheme
 
-/**
- * A regular preview using the default theme - which is a
- * light theme
- */
-@Preview(
-    group = "Individual rows"
-)
+@LightDarkThemePreview
 @Composable
 fun BorrowedItemRowPreview(
     @PreviewParameter(BorrowItemsProvider::class) item: BorrowItem
@@ -22,28 +16,6 @@ fun BorrowedItemRowPreview(
     BorrowTheme {
         BorrowedItemRow(
             item
-        )
-    }
-}
-
-/**
- * Preview the Composable using the dark theme colors of the
- * BorrowTheme theme
- */
-@Preview(
-    group = "Individual rows",
-    uiMode = Configuration.UI_MODE_NIGHT_YES
-)
-@Composable
-fun BorrowedItemRowDarkPreview() {
-    BorrowTheme {
-        BorrowedItemRow(
-            BorrowItem(
-                id = 0,
-                itemName = "PlayStation",
-                borrowerName = "Steve",
-                borrowDate = "2-11-2022"
-            )
         )
     }
 }
